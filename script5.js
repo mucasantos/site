e544b01967eff0ad2e49899edc8fa1d066cf3af4
Samuel Santos <juca@email.com>
2023-06-29 20:35:10 -0300

    classes update june

--- script5.js
+++ script5.js
@@ -1,26 +1,26 @@
+//Aula 03 Javscript básico
+//Funções e manipulação DOM
+
 var valor1 = document.getElementById("valor1");
 var valor2 = document.getElementById("valor2");
+var mostrarResultado = document.getElementById("result")
 
 function somar() {
   var resultado = Number(valor1.value) + Number(valor2.value);
-
-  document.querySelector('h3').textContent = resultado
+  mostrarResultado.textContent = resultado;
 }
 
 function subtrair() {
-    var resultado = Number(valor1.value) - Number(valor2.value);
-  
-    document.querySelector('h3').textContent = resultado
-  }
+  var resultado = Number(valor1.value) - Number(valor2.value);
+  mostrarResultado.textContent = resultado;
+}
 
-  function multiplicar() {
-    var resultado = Number(valor1.value) * Number(valor2.value);
-  
-    document.querySelector('h3').textContent = resultado
-  }
+function multiplicar() {
+  var resultado = Number(valor1.value) * Number(valor2.value);
+  mostrarResultado.textContent = resultado;
+}
 
-  function somar() {
-    var resultado = Number(valor1.value) + Number(valor2.value);
-  
-    document.querySelector('h3').textContent = resultado
-  }
+function dividir() {
+  var resultado = Number(valor1.value) / Number(valor2.value);
+  mostrarResultado.textContent = resultado;
+}
